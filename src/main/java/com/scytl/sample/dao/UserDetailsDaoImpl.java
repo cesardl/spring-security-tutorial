2a8edcea7a22f719189c1b778b6000a672f421a5
César Díaz <cesar.diaz@scytl.com>
2015-08-07 20:38:00 -0500

    Adding scanning

--- src/main/java/com/scytl/sample/dao/UserDetailsDaoImpl.java
+++ src/main/java/com/scytl/sample/dao/UserDetailsDaoImpl.java
@@ -46,22 +46,21 @@ public class UserDetailsDaoImpl extends JdbcDaoSupport implements UserDetailsDao
         if (user == null) {
             if (isUserExists(username)) {
                 // if no record, insert a new
-                getJdbcTemplate().update(SQL_USER_ATTEMPTS_INSERT, new Object[] { username, 1, new Date() });
+                getJdbcTemplate().update(SQL_USER_ATTEMPTS_INSERT, new Object[]{username, 1, new Date()});
             }
         } else {
 
             if (isUserExists(username)) {
                 // update attempts count, +1
-                getJdbcTemplate().update(SQL_USER_ATTEMPTS_UPDATE_ATTEMPTS, new Object[] { new Date(), username});
+                getJdbcTemplate().update(SQL_USER_ATTEMPTS_UPDATE_ATTEMPTS, new Object[]{new Date(), username});
             }
 
             if (user.getAttempts() + 1 >= MAX_ATTEMPTS) {
                 // locked user
-                getJdbcTemplate().update(SQL_USERS_UPDATE_LOCKED, new Object[] { false, username });
+                getJdbcTemplate().update(SQL_USERS_UPDATE_LOCKED, new Object[]{false, username});
                 // throw exception
                 throw new LockedException("User Account is locked!");
             }
-
         }
     }
 
@@ -69,9 +68,9 @@ public class UserDetailsDaoImpl extends JdbcDaoSupport implements UserDetailsDao
     public UserAttempts getUserAttempts(String username) {
         try {
             UserAttempts userAttempts = getJdbcTemplate().queryForObject(SQL_USER_ATTEMPTS_GET,
-                    new Object[] { username }, new RowMapper<UserAttempts>() {
+                    new Object[]{username}, new RowMapper<UserAttempts>() {
+                        @Override
                         public UserAttempts mapRow(ResultSet rs, int rowNum) throws SQLException {
-
                             UserAttempts user = new UserAttempts();
                             user.setId(rs.getInt("id"));
                             user.setUsername(rs.getString("username"));
@@ -80,7 +79,6 @@ public class UserDetailsDaoImpl extends JdbcDaoSupport implements UserDetailsDao
 
                             return user;
                         }
-
                     });
             return userAttempts;
 
@@ -92,14 +90,14 @@ public class UserDetailsDaoImpl extends JdbcDaoSupport implements UserDetailsDao
     @Override
     public void resetFailAttempts(String username) {
         getJdbcTemplate().update(
-                SQL_USER_ATTEMPTS_RESET_ATTEMPTS, new Object[] { username });
+                SQL_USER_ATTEMPTS_RESET_ATTEMPTS, new Object[]{username});
     }
 
     private boolean isUserExists(String username) {
         boolean result = false;
 
         int count = getJdbcTemplate().queryForObject(
-                SQL_USERS_COUNT, new Object[] { username }, Integer.class);
+                SQL_USERS_COUNT, new Object[]{username}, Integer.class);
         if (count > 0) {
             result = true;
         }
--- src/main/java/com/scytl/sample/web/controller/LoginController.java
+++ src/main/java/com/scytl/sample/web/controller/LoginController.java
@@ -1,10 +1,13 @@
-package com.scytl.sample.methodleveljsr250;
+package com.scytl.sample.web.controller;
 
 import java.security.Principal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -37,12 +40,16 @@ public class LoginController {
     }
 
     @RequestMapping(value = {"/", "/login"}, method = RequestMethod.GET)
-    public ModelAndView login(@RequestParam(value = "error", required = false) String error) {
+    public ModelAndView login(
+            @RequestParam(value = "error", required = false) String error,
+            HttpServletRequest request) {
 
         ModelAndView model = new ModelAndView();
         if (error != null) {
             log.info("error {}", error);
-            model.addObject("error", "Invalid username and password!");
+            // model.addObject("error", "Invalid username and password!");
+            model.addObject("error",
+                    getErrorMessage(request, "SPRING_SECURITY_LAST_EXCEPTION"));
         }
 
         model.setViewName("login_page");
@@ -85,6 +92,23 @@ public class LoginController {
         return "login_page";
     }
 
+    //customize the error message
+    private String getErrorMessage(HttpServletRequest request, String key) {
+        Exception exception
+                = (Exception) request.getSession().getAttribute(key);
+
+        String error;
+        if (exception instanceof BadCredentialsException) {
+            error = "Invalid username and password!";
+        } else if (exception instanceof LockedException) {
+            error = exception.getMessage();
+        } else {
+            error = "Invalid username and password!";
+        }
+
+        return error;
+    }
+
     // for 403 access denied page
     @RequestMapping(value = "/403", method = RequestMethod.GET)
     public ModelAndView accesssDenied() {
@@ -94,6 +118,8 @@ public class LoginController {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (!(auth instanceof AnonymousAuthenticationToken)) {
             UserDetails userDetail = (UserDetails) auth.getPrincipal();
+            log.info("{}", userDetail);
+
             model.addObject("username", userDetail.getUsername());
         }
 
